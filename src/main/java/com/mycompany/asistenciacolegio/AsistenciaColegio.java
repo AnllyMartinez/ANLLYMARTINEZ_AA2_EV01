2b06ec7bea7436ec0664fb18f5dd4b07984c56c2
Anlly Martinez <anlly123@gmail.com>
2024-07-27 18:22:36 -0500

    Se agrega comentarios

--- src/main/java/com/mycompany/asistenciacolegio/AsistenciaColegio.java
+++ src/main/java/com/mycompany/asistenciacolegio/AsistenciaColegio.java
@@ -10,8 +10,8 @@ public class AsistenciaColegio {
 
     public static class BasedatosConexion {
         private static final String URL = "jdbc:mysql://localhost:3306/bdcontrolasistencia";
-        private static final String USUARIO = "root"; // Usuario por defecto
-        private static final String PASSWORD = ""; // Contraseña vacía
+        private static final String USUARIO = "root"; // Usuario 
+        private static final String PASSWORD = ""; 
 
         public static Connection getConnection() {
             Connection conexion = null;
@@ -20,9 +20,17 @@ public class AsistenciaColegio {
             try {
                 conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
                 statement = conexion.createStatement();
+              //Esta parte es para saber si esta funcionando correctamente, van a ser cambiados posteriormente. 
                 rs = statement.executeQuery("SELECT * FROM estudiante");
-                while(rs.next()){
+                 while(rs.next()){
                     System.out.println(rs.getString("estudianteId"));
+                }
+                // Insercion de datos
+                statement.execute("INSERT INTO `usuario` (`idUsuario`, `nombre`, `horario`, `rol`) VALUES ('0002', 'Nicolas', 'Lunes 8:30-16:30', 'Estudiante');");
+                // Mostrar el dato para saber si se inserto. 
+                rs = statement.executeQuery("SELECT * FROM usuario");
+                while(rs.next()){
+                    System.out.println(rs.getString("nombre"));
                 }
             } catch (SQLException e) {
                 System.err.println("Error al conectar a la base de datos: " + e.getMessage());
